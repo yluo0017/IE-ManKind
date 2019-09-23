75a186a2f26307a4b0b2a486c7fa9d25ec3c9294
yluo0017 <43309025+yluo0017@users.noreply.github.com>
2019-09-23 15:00:45 +1000

    V3.01

--- app/src/main/java/com/example/mankind/NavigationActivity.java
+++ app/src/main/java/com/example/mankind/NavigationActivity.java
@@ -2,7 +2,13 @@ package com.example.mankind;
 
 import android.app.ActionBar;
 import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
 import android.content.SharedPreferences;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
@@ -16,7 +22,9 @@ import androidx.navigation.ui.NavigationUI;
 /**
  * The type Navigation activity.
  */
-public class NavigationActivity extends AppCompatActivity {
+public class NavigationActivity extends AppCompatActivity implements SensorEventListener {
+	private SensorManager sensorManager;
+	private Sensor accelerateSensor;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -29,5 +37,37 @@ public class NavigationActivity extends AppCompatActivity {
 		NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
 		NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
 		NavigationUI.setupWithNavController(navView, navController);
+		initSensor();
+	}
+
+	private void initSensor() {
+		sensorManager = (SensorManager) this.getSystemService(Context.SENSOR_SERVICE);
+		accelerateSensor = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+		sensorManager.registerListener(this, accelerateSensor, SensorManager.SENSOR_DELAY_NORMAL);
+	}
+
+	@Override
+	protected void onPause() {
+		if (sensorManager != null) {
+			sensorManager.unregisterListener(this);
+		}
+		super.onPause();
+	}
+
+	@Override
+	public void onSensorChanged(SensorEvent event) {
+		float[] values = event.values;
+		float x = values[0];
+		float y = values[1];
+		float z = values[2];
+
+		if ((Math.abs(x) > 15 || Math.abs(y) > 15 || Math
+				.abs(z) > 15)){
+		}
+	}
+
+	@Override
+	public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
 	}
 }
